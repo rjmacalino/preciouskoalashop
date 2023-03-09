9d5999238879466b7a8ae042cb6d1ac6a555dcd0
rjmacalino <rjhaymacalino@gmail.com>
2023-03-09 13:28:05 +1100

    live mode change to price front end

--- src/app/components/shop/Shop.jsx
+++ src/app/components/shop/Shop.jsx
@@ -16,7 +16,7 @@ const Shop = () => {
     <div className={style.container}>
       <h1 className={style.title}>Breast Milk Storage Bags</h1>
       <div className={style.image_div}><Image src={img1} className={style.main_image} alt='main-image'></Image></div>
-      <h2 className={style.price}>$50</h2>
+      <h2 className={style.price}>$9.99 AUD</h2>
       <h2 className={style.description}>
         Precious Koala Breast Milk Storage Bags are designed to make storing and freezing breast milk easy and convenient. The bags are made from durable, food-grade, BPA-free plastic that is safe for your baby. Each bag has a double-zipper seal to prevent leaks and a write-on tab to help you label and organize your milk.
       </h2>
